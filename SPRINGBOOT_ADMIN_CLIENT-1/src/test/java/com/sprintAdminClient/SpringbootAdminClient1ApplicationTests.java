cb3a4e0fa9e453e767abb617a95383438d17f884
krishkhk <92845736+krishkhk@users.noreply.github.com>
2022-09-08 10:47:07 +0530

    project

---
+++ SPRINGBOOT_ADMIN_CLIENT-1/src/test/java/com/sprintAdminClient/SpringbootAdminClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintAdminClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminClient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
